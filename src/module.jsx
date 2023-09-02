6ed927aee3ed0b148a91227de98d24e088f0f0f0
lexand41 <lexand41@yandex.ru>
2023-09-02 14:39:02 +0700

    first commit

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from './img/logo41.jpg';
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img width={200} src={logo} />
+  </>
+);
+
+export default Module;
